3519b1165ebb4217b9edff07c610edcdd7c98506
mal1992 <malavikah@malavikas-MacBook-Pro.local>
2015-02-04 14:17:37 -0500

    First Try at Twilio Integration

---
+++ TwilioWebAPP-SMS/default.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="TwilioWebAPP_SMS._default" %>
+
+
+
+  <div>
+                <form>
+                <h4>Number</h4>
+                <input type="text" ID="ToNumberr" TextMode="Password" runat="server" />
+                 <h4>Message</h4>
+                <input type="text" ID="Messagetxt"  runat="server" />
+                <br />
+                <br />
+                <button type="submit"  OnClick="SendMessage_OnClick" />
+                   </form>
+            </div>
+
+
+
+
---
+++ TwilioWebAPP-SMS/default.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using Twilio;
+namespace TwilioWebAPP_SMS
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        protected void SendMessage_OnClick()
+        {
+            string ACCOUNT_ID = "AC0fe6b30e4caba008476174e49fb868af";
+            string AUTH_TOKEN = "e0b98f8ab5c8df323b4cc277d9ff5340";
+
+            TwilioRestClient client = new TwilioRestClient(ACCOUNT_ID, AUTH_TOKEN);
+            client.SendSmsMessage("(650) 651-4864", ToNumberr.ToString(), Messagetxt.ToString());
+        }
+    }
+}
